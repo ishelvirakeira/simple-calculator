9767631977ad7fcc76aceec53cb431780544c327
Elvira Keira Ishimwe <97549159+ishelvirakeira@users.noreply.github.com>
2025-09-25 12:10:29 -0400

    main.js

--- main.js
+++ main.js
@@ -8,21 +8,26 @@ const prodres=document.getElementById('prod');
 const quotres=document.getElementById('quot');
 
 form.addEventListener('submit', (e)=>{
-    e.preventDefault();
+    e.preventDefault();//inspo from the  JavaScript DOM Crash Course
 
-    const a=parseFloat(x.value);
+    const a=parseFloat(x.value);//referenced from mdn
     const b = parseFloat(y.value);
 
-    if (Number.isNaN(a) || Number.isNaN(b)){
+    if (Number.isNaN(a) || Number.isNaN(b)){//mdn
         alert('Please enter two numbers.');
-        (Number.isNaN(a) ? x:y).focus();
-        return;
+        if (Number.isNaN(a)){
+            x.focus();
+        } else {
+            y.focus();
+        }
     }
-
-    sumres.textContent=a+b;
-    diffres.textContent=a-b;
-    prodres.textContent=a*b;
-    quotres.textContent=(b===0)? 'Cannot divide by 0': (a/b);
-
-    res.classList.remove('hidden');
+ 
+    sumres.innerHTML=a+b;
+    diffres.innerHTML=a-b;
+    prodres.innerHTML=a*b;
+    if(b===0){
+        quotres.innerHTML= "Cannot divide by 0";
+    } else {
+        quotres.innerHTML = a/b;
+    }
 })
